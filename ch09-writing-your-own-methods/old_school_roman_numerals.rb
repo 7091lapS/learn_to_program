18b4a4188cc6afb37e8090cd57cfb9114b22cfaf
Antonio Voce <antonio.v0c3@gmail.com>
2015-07-08 18:10:29 +0200

    completed old_school_roman_numerals.rb

--- ch09-writing-your-own-methods/old_school_roman_numerals.rb
+++ ch09-writing-your-own-methods/old_school_roman_numerals.rb
@@ -1,3 +1,16 @@
 def old_roman_numeral num
-  # your code here
+  if num > 0 && num < 3001
+    nM = num / 1000
+    nD = (num % 1000) / 500
+    nC = (num % 500) / 100
+    nL = (num % 100) / 50
+    nX = (num % 50) / 10
+    nV = (num % 10) / 5
+    nI = num % 5
+
+    return "M" * nM + "D" * nD + "C" * nC + "L" * nL + "X" * nX + "V" * nV + "I" * nI
+  else
+    puts "use a number between 0 and 3000"
+  end
 end
+puts old_roman_numeral 3001
